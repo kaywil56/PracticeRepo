a3d40249c3593127938396f13f1369379892ffdd
Kaylem Williams Sutherland (1000050830) <WILLKJ8@student.op.ac.nz>
2020-10-14 09:24:18 +1300

    First Update

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+
+namespace Practice
+{
+    class Program
+    {
+        public static void method1()
+        {
+            string word;
+            Console.WriteLine("Enter A Word");
+            word = Console.ReadLine();
+            Console.WriteLine($"Your word is {word}");
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            method1();
+           
+
+
+           
+        }
+    }
+}
